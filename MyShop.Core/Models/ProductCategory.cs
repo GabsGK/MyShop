2ef7526bd02c678013404bf0a6c00c035dc44d6c
Gabriela Gonzalez <gabriela.gonzalez@chas.se>
2021-04-08 11:55:33 +0200

    Product Grouping adding

---
+++ MyShop.Core/Models/ProductCategory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+	public class ProductCategory
+	{
+		public string Id { get; set; }
+		public string Category { get; set; }
+		
+		//Below the constructor, in where we generate the id everytime a product is created.
+		public ProductCategory()
+		{
+			this.Id = Guid.NewGuid().ToString();
+		}
+	}
+}
