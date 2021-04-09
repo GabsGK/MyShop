c58ba63f769b40be45a33b4f6d337decca561ebd
Gabriela Gonzalez <gabriela.gonzalez@chas.se>
2021-04-09 10:18:47 +0200

    Added Entity Framework

---
+++ MySjop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySjop.DataAccess.SQL
+{
+	public class DataContext : DbContext
+	{
+		/*With the code below we are now able to create our data base.
+		 * Entity FW Migration Classes are responsible for connecting
+		 * to the DB and creating or updating the tables. We use a series
+		 * of special commands executed in the command window called
+		 * Package manager console */
+		public DataContext()
+			:base("DefaultConnection"){
+
+		}
+
+
+		public DbSet<Product> Products { get; set; }
+		public DbSet<ProductCategory> ProductCategories { get; set; }
+	}
+}
